fbeaf3a1d1b4a21763d340f0cd414f9915ba915b
Fluency <cliu226@jh.edu>
2025-07-29 12:30:17 -0400

    add contact annotation scripts

--- src/dvrk_data_processing/kinematic_mapping/gen_kinematic_heatmap.py
+++ src/dvrk_data_processing/kinematic_mapping/gen_kinematic_heatmap.py
@@ -112,6 +112,8 @@ p_config = Path.cwd().parents[2] / 'config'
     config_name="config_kp"
 )
 def main(cfg: AppCfg):
+
+
     camera_calibration_path = Path(cfg.camera_calibration_path)
     camera_names = cfg.camera_names
     camera_offset = cfg.camera_offset
@@ -149,7 +151,10 @@ def main(cfg: AppCfg):
 
     data_folder = input_folder / 'kinematic'
     save_folder = output_folder
-
+    save_root_dvrk    = output_folder.parent / f"{output_folder.name}_dVRK"
+    save_root_handeye = output_folder.parent / f"{output_folder.name}_handeye"
+    for p in [save_root_dvrk, save_root_handeye]:
+        p.mkdir(parents=True, exist_ok=True)
     for i_cam in range(len(camera_names)):
         camera_file_path = camera_calibration_path / f'{camera_names[i_cam]}.yaml'
         if len(camera_names) == 2:
@@ -175,10 +180,20 @@ def main(cfg: AppCfg):
             file_list = glob_sorted_frame(data_path)
 
             if camera_names is not None:
-                arm_save_folder = save_folder / arm_name / camera_names[i_cam]
+                arm_save_dvrk    = save_root_dvrk    / arm_name / camera_names[i_cam]
+                arm_save_he      = save_root_handeye / arm_name / camera_names[i_cam]
+                for p in [arm_save_dvrk, arm_save_he]:
+                    (p / "image").mkdir(parents=True, exist_ok=True)
+                    (p / "heatmap").mkdir(parents=True, exist_ok=True)
+
                 print(f'Working on {camera_names[i_cam].upper()} Camera: \n')
             else:
-                arm_save_folder = save_folder / arm_name
+                arm_save_dvrk    = save_root_dvrk    /  camera_names[i_cam]
+                arm_save_he      = save_root_handeye /  camera_names[i_cam]
+                for p in [arm_save_dvrk, arm_save_he]:
+                    (p / "image").mkdir(parents=True, exist_ok=True)
+                    (p / "heatmap").mkdir(parents=True, exist_ok=True)
+
 
             for file_name in tqdm(file_list, desc=f"Kinematic HeatMap Processing Frames of {arm_name}"):
                 # === Camera←World  ===
@@ -194,12 +209,12 @@ def main(cfg: AppCfg):
                     img_greyscale = cv2.imread(str(img_file_path), cv2.IMREAD_GRAYSCALE).astype(np.float64)
                 img_file_name = file_name.parts[-1].replace('json', 'png')
                 heatmap_file_name = file_name.parts[-1].replace('json', 'npy')
-                img_save_folder = arm_save_folder / 'image'
-                heatmap_save_folder = arm_save_folder / 'heatmap'
-                if not img_save_folder.exists():
-                    create_folder(img_save_folder)
-                if not heatmap_save_folder.exists():
-                    create_folder(heatmap_save_folder)
+                # img_save_folder = arm_save_folder / 'image'
+                # heatmap_save_folder = arm_save_folder / 'heatmap'
+                # if not img_save_folder.exists():
+                #     create_folder(img_save_folder)
+                # if not heatmap_save_folder.exists():
+                #     create_folder(heatmap_save_folder)
 
                 # R = data_arm.R
                 t = data_arm.t
@@ -245,18 +260,26 @@ def main(cfg: AppCfg):
                 )
 
                 # ---------- heatmap addup ----------
-                kp_heat = kp_heat + kp_heat_he
+                # kp_heat = kp_heat + kp_heat_he
 
 
                 if enable_overlay:
                     kp_heat = np.multiply(kp_heat, img_greyscale)
+                    kp_heat_he = np.multiply(kp_heat_he, img_greyscale)
+                # heatmap_file_path = heatmap_save_folder / heatmap_file_name
+                
+                # -------- dVRK --------
+                np.save(arm_save_dvrk / "heatmap" / heatmap_file_name, kp_heat)
+                kp_norm_dvrk = cv2.normalize(kp_heat, None, 0, 255,
+                                            cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                cv2.imwrite(str(arm_save_dvrk / "image" / img_file_name), kp_norm_dvrk)
 
-                heatmap_file_path = heatmap_save_folder / heatmap_file_name
-                np.save(heatmap_file_path, kp_heat)
+                # -------- Hand-Eye ----
+                np.save(arm_save_he / "heatmap" / heatmap_file_name, kp_heat_he)
+                kp_norm_he = cv2.normalize(kp_heat_he, None, 0, 255,
+                                        cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                cv2.imwrite(str(arm_save_he / "image" / img_file_name), kp_norm_he)
 
-                img_file_path = img_save_folder / img_file_name
-                kp_norm = cv2.normalize(kp_heat, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-                cv2.imwrite(str(img_file_path), kp_norm)
 
                 # -----------------------------------------------------------
                 #  DEBUG: print/record 3-D and pixel data  (every 50 frames)
