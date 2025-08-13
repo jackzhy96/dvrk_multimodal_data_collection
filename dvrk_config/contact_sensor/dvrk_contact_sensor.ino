a99089fc1469b1ad5ec8f9ab5aaa684cf343d13b
saramartuscelli <sara.martuscelli@mail.polimi.it>
2025-08-13 00:43:18 -0400

    Add arduino and XML codes

---
+++ dvrk_config/contact_sensor/dvrk_contact_sensor.ino
@@ -0,0 +1,60 @@
+#include <CapacitiveSensor.h>
+
+#define SEND_PIN 2
+#define RECEIVE_PIN_1 4
+#define RECEIVE_PIN_2 8
+#define RECEIVE_PIN_3 12
+
+#define OUTPUT_PIN_1 5
+#define OUTPUT_PIN_2 9
+#define OUTPUT_PIN_3 13   // using pin 13 allows you to visualize you output with the arduino built-in LED
+
+
+CapacitiveSensor psm1Sensor = CapacitiveSensor(SEND_PIN, RECEIVE_PIN_1);
+CapacitiveSensor psm2Sensor = CapacitiveSensor(SEND_PIN, RECEIVE_PIN_2);
+CapacitiveSensor psm3Sensor = CapacitiveSensor(SEND_PIN, RECEIVE_PIN_3);
+
+void setup() {
+  //psm1Sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on PSM1
+  pinMode(OUTPUT_PIN_1, OUTPUT);
+  pinMode(OUTPUT_PIN_2, OUTPUT);
+  pinMode(OUTPUT_PIN_3, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  long value1 = psm1Sensor.capacitiveSensor(30);
+  long value2 = psm2Sensor.capacitiveSensor(30);
+  long value3 = psm3Sensor.capacitiveSensor(30);
+
+  // uncomment the following lines to see the output of you sensor and set the threshold
+  // Serial.print(millis() - start);        // check on performance in milliseconds
+  // long start = millis();
+  // Serial.print("\t");                    
+  // Serial.print(value1);                  // print sensor1 output 
+  // Serial.print("\t");                    
+  // Serial.print(value2);                  // print sensor2 output 
+  // Serial.print("\t");                    
+  // Serial.println(value3);                // print sensor3 output 
+
+  int threshold = 200;
+  
+  if(value1>threshold) digitalWrite(OUTPUT_PIN_1, HIGH);
+  else digitalWrite(OUTPUT_PIN_1, LOW);
+
+  if(value2>threshold) digitalWrite(OUTPUT_PIN_2, HIGH);
+  else digitalWrite(OUTPUT_PIN_2, LOW);
+
+  if(value3>threshold) digitalWrite(OUTPUT_PIN_3, HIGH);
+  else digitalWrite(OUTPUT_PIN_3, LOW);
+
+  // uncomment the following lines to see how your binarization performs
+  // Serial.print(digitalRead(PSM1_PIN));                  
+  // Serial.print("\t");                    
+  // Serial.print(digitalRead(PSM2_PIN));                  
+  // Serial.print("\t");                    
+  // Serial.println(digitalRead(PSM3_PIN));
+
+  delay(100);
+}
