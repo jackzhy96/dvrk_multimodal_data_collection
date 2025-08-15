8badf8148d21549faf38eaf0f5006ae2aeccbf0a
saramartuscelli <sara.martuscelli@mail.polimi.it>
2025-08-15 14:53:54 -0400

    update arduino code

--- dvrk_config/contact_sensor/dvrk_contact_sensor.ino
+++ dvrk_config/contact_sensor/dvrk_contact_sensor.ino
@@ -15,7 +15,9 @@ CapacitiveSensor psm2Sensor = CapacitiveSensor(SEND_PIN, RECEIVE_PIN_2);
 CapacitiveSensor psm3Sensor = CapacitiveSensor(SEND_PIN, RECEIVE_PIN_3);
 
 void setup() {
-  //psm1Sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on PSM1
+  psm1Sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on sensor 1
+  psm2Sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);	    // turn off autocalibrate on sensor 2
+  psm3Sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on sensor 3
   pinMode(OUTPUT_PIN_1, OUTPUT);
   pinMode(OUTPUT_PIN_2, OUTPUT);
   pinMode(OUTPUT_PIN_3, OUTPUT);
@@ -24,13 +26,14 @@ void setup() {
 
 void loop() {
   
-  long value1 = psm1Sensor.capacitiveSensor(30);
-  long value2 = psm2Sensor.capacitiveSensor(30);
-  long value3 = psm3Sensor.capacitiveSensor(30);
+  long value1 = psm1Sensor.capacitiveSensor(10);
+  long value2 = psm2Sensor.capacitiveSensor(10);
+  long value3 = psm3Sensor.capacitiveSensor(10);
 
   // uncomment the following lines to see the output of you sensor and set the threshold
-  // Serial.print(millis() - start);        // check on performance in milliseconds
   // long start = millis();
+  // Serial.print(millis() - start);        // check on performance in milliseconds
+  
   // Serial.print("\t");                    
   // Serial.print(value1);                  // print sensor1 output 
   // Serial.print("\t");                    
@@ -38,7 +41,7 @@ void loop() {
   // Serial.print("\t");                    
   // Serial.println(value3);                // print sensor3 output 
 
-  int threshold = 200;
+  int threshold = 105;
   
   if(value1>threshold) digitalWrite(OUTPUT_PIN_1, HIGH);
   else digitalWrite(OUTPUT_PIN_1, LOW);
@@ -49,12 +52,12 @@ void loop() {
   if(value3>threshold) digitalWrite(OUTPUT_PIN_3, HIGH);
   else digitalWrite(OUTPUT_PIN_3, LOW);
 
-  // uncomment the following lines to see how your binarization performs
-  // Serial.print(digitalRead(PSM1_PIN));                  
+  //uncomment the following lines to see how your binarization performs
+  // Serial.print(digitalRead(OUTPUT_PIN_1));                  
   // Serial.print("\t");                    
-  // Serial.print(digitalRead(PSM2_PIN));                  
+  // Serial.print(digitalRead(OUTPUT_PIN_2));                  
   // Serial.print("\t");                    
-  // Serial.println(digitalRead(PSM3_PIN));
+  // Serial.println(digitalRead(OUTPUT_PIN_3));
 
-  delay(100);
+  delay(10);
 }
